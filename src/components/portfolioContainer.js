c79e76932a81a371d5007be3139497f931c56e16
Eduardo Coello <jeck24@hotmail.com>
2021-03-08 18:55:28 -0600

    portfolioContainer.js created and populated

---
+++ src/components/portfolioContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import NavTab from "./navtab";
+import Index from "./index";
+import Portfolio from "./portfolio";
+import Contact from "./contact";
+
+class Container extends Component {
+  state = {
+    currentPage: "Index"
+  };
+
+  handlePageChange = page => {
+    this.setState({ currentPage: page });
+  };
+
+  renderPage = () => {
+    if (this.state.currentPage === "Index") {
+      return <Index />;
+    } else if (this.state.currentPage === "Portfolio") {
+      return <Portfolio />;
+    } else {
+      return <Contact />;
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTab
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange}
+        />
+        {this.renderPage()}
+      </div>
+    );
+  }
+}
+
+export default Container;
